32f0de146cd6b8afb034a08b3397dce34565c755
Spencer Ong <spencer.pg@gmail.com>
2015-07-12 23:05:04 +0100

    keyboard/mouse input for FPS style camera

--- opengl_study/main.cpp
+++ opengl_study/main.cpp
@@ -6,8 +6,11 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+
 
 GLFWwindow* window = 0;
 GLuint program = 0;
@@ -17,28 +20,10 @@ GLuint tex = 0;
 cv::Mat img;
 bool swap_col = false;
 
-static void render_scene()
-{
-	glClearColor(0, 0, 0, 1); // black
-
-	glClear(GL_COLOR_BUFFER_BIT);
-	
-	glUseProgram(program);
-	
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, tex);
-	glUniform1i(glGetUniformLocation(program, "tex"), 0);
-	
-	glBindVertexArray(g_VAO);
-
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-
-	glBindVertexArray(0);
-	glBindTexture(GL_TEXTURE_2D, 0);
-	glUseProgram(0);
-
-	glfwSwapBuffers(window);
-}
+glm::vec3 cam_pos;
+float cam_vAngle = 0.0f, cam_hAngle = 0.0f;
+double cam_zoom = 0.0;
+float cam_fov = 50.0f;
 
 static GLuint read_shader(const char* filename, GLenum shader_type)
 {
@@ -125,9 +110,53 @@ static void load_scene()
 	// Put the three triangle verticies into the VBO
 	GLfloat vertexData[] = {
 		//  X     Y     Z       U     V
-		0.0f, 0.8f, 0.0f, 0.5f, 1.0f,
-		-0.8f, -0.8f, 0.0f, 0.0f, 0.0f,
-		0.8f, -0.8f, 0.0f, 1.0f, 0.0f,
+		// bottom
+		-1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
+		1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
+		-1.0f, -1.0f, 1.0f, 0.0f, 1.0f,
+		1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
+		1.0f, -1.0f, 1.0f, 1.0f, 1.0f,
+		-1.0f, -1.0f, 1.0f, 0.0f, 1.0f,
+
+		// top
+		-1.0f, 1.0f, -1.0f, 0.0f, 0.0f,
+		-1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+		1.0f, 1.0f, -1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, -1.0f, 1.0f, 0.0f,
+		-1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+		1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+
+		// front
+		-1.0f, -1.0f, 1.0f, 1.0f, 0.0f,
+		1.0f, -1.0f, 1.0f, 0.0f, 0.0f,
+		-1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+		1.0f, -1.0f, 1.0f, 0.0f, 0.0f,
+		1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+
+		// back
+		-1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
+		-1.0f, 1.0f, -1.0f, 0.0f, 1.0f,
+		1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
+		1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
+		-1.0f, 1.0f, -1.0f, 0.0f, 1.0f,
+		1.0f, 1.0f, -1.0f, 1.0f, 1.0f,
+
+		// left
+		-1.0f, -1.0f, 1.0f, 0.0f, 1.0f,
+		-1.0f, 1.0f, -1.0f, 1.0f, 0.0f,
+		-1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
+		-1.0f, -1.0f, 1.0f, 0.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, -1.0f, 1.0f, 0.0f,
+
+		// right
+		1.0f, -1.0f, 1.0f, 1.0f, 1.0f,
+		1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, -1.0f, 0.0f, 0.0f,
+		1.0f, -1.0f, 1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, -1.0f, 0.0f, 0.0f,
+		1.0f, 1.0f, 1.0f, 0.0f, 1.0f
 	};
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
 
@@ -145,7 +174,7 @@ static void load_scene()
 
 static void load_textures()
 {
-	cv::Mat src_img = cv::imread("hazard.png", CV_LOAD_IMAGE_COLOR);
+	cv::Mat src_img = cv::imread("wooden-crate.jpg", CV_LOAD_IMAGE_COLOR);
 	cv::flip(src_img, img, 0);
 
 	glGenTextures(1, &tex);
@@ -166,8 +195,92 @@ static void load_textures()
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
+static void render_scene()
+{
+	glClearColor(0, 0, 0, 1); // black
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(program);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glUniform1i(glGetUniformLocation(program, "tex"), 0);
+
+	glm::mat4 camera = glm::perspective(glm::radians(cam_fov), 800 / 600.0f, 0.1f, 10.0f);
+	
+	glm::mat4 orientation;
+	orientation = glm::rotate(orientation, glm::radians(cam_vAngle), glm::vec3(1, 0, 0));
+	orientation = glm::rotate(orientation, glm::radians(cam_hAngle), glm::vec3(0, 1, 0));
+	camera *= orientation;
+
+	camera = glm::translate(camera, -cam_pos);
+
+	//glm::mat4 camera = glm::lookAt(cam_pos, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glUniformMatrix4fv(glGetUniformLocation(program, "camera"), 1, GL_FALSE, glm::value_ptr(camera));
+
+	float box_rot_angle = (float) glfwGetTime() * 45;
+	glm::mat4 model = glm::rotate(glm::mat4(), glm::radians(box_rot_angle), glm::vec3(0, 1, 0));
+	glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+	glm::vec3 forward = glm::vec3(glm::inverse(orientation) * glm::vec4(0, 0, -1, 1));
+	glm::vec3 up = glm::vec3(glm::inverse(orientation) * glm::vec4(0, 1, 0, 1));
+	glm::vec3 right = glm::vec3(glm::inverse(orientation) * glm::vec4(1, 0, 0, 1));
+
+	if (glfwGetKey(window, 'S')){
+		cam_pos -= 0.1f * forward;
+	}
+	else if (glfwGetKey(window, 'W')){
+		cam_pos += 0.1f * forward;
+	}
+	if (glfwGetKey(window, 'A')){
+		cam_pos -= 0.1f * right;
+	}
+	else if (glfwGetKey(window, 'D')){
+		cam_pos += 0.1f * right;
+	}
+	if (glfwGetKey(window, 'X')){
+		cam_pos -= 0.1f * up;
+	}
+	else if (glfwGetKey(window, 'Z')){
+		cam_pos += 0.1f * up;
+	}
+
+	double mouseX, mouseY;
+	glfwGetCursorPos(window, &mouseX, &mouseY);
+	cam_vAngle += 0.1f * (float)mouseY;
+	cam_hAngle += 0.1f * (float)mouseX;
+
+	cam_hAngle = fmodf(cam_hAngle, 360.0f);
+	cam_vAngle = (cam_vAngle > 85.0f) ? 85.0f: cam_vAngle;
+	cam_vAngle = (cam_vAngle < -85.0f) ? -85.0f : cam_vAngle;
+	glfwSetCursorPos(window, 0, 0);
+
+	const float zoomSensitivity = -1.5f;
+	float fieldOfView = cam_fov + zoomSensitivity * (float)cam_zoom;
+	if (fieldOfView < 5.0f) fieldOfView = 5.0f;
+	if (fieldOfView > 130.0f) fieldOfView = 130.0f;
+	
+	cam_fov = fieldOfView;
+	cam_zoom = 0;
+
+	glBindVertexArray(g_VAO);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3*2*6);
+
+	glBindVertexArray(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glUseProgram(0);
+
+	glfwSwapBuffers(window);
+}
+
 void on_error(int error_code, const char* msg) {
 	throw std::runtime_error(msg);
+}
+
+void OnScroll(GLFWwindow* window, double deltaX, double deltaY) {
+	cam_zoom += deltaY;
 }
 
 void dain()
@@ -191,10 +304,17 @@ void dain()
 	// GLFW settings
 	glfwMakeContextCurrent(window);
 
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPos(window, 0, 0);
+	glfwSetScrollCallback(window, OnScroll);
+
 	// initialise GLEW
 	glewExperimental = GL_TRUE; //stops glew crashing on OSX :-/
 	if (glewInit() != GLEW_OK)
 		throw std::runtime_error("glewInit failed");
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
 
 	// print out some info about the graphics drivers
 	std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
@@ -218,6 +338,9 @@ void dain()
 
 		// draw one frame
 		render_scene();
+
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE))
+			glfwSetWindowShouldClose(window, GL_TRUE);
 	}
 
 	// clean up and exit
